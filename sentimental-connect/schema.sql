6b7632ef2b5e8b1a7d1e2b5815898cd1e9af0290
lin <a6491080230@gmail.com>
2024-08-12 23:26:41 +0800

    Update

--- sentimental-connect/schema.sql
+++ sentimental-connect/schema.sql
@@ -6,21 +6,63 @@ USE `linkedin`;
 
 -- check50 cs50/problems/2024/sql/sentimental/connect
 
-CREATE TABLE `Users` (
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` INT AUTO_INCREMENT,
     `first_name` VARCHAR(32) NOT NULL,
     `last_name` VARCHAR(32) NOT NULL,
     `username` VARCHAR(32) NOT NULL UNIQUE,
-    `password` VARCHAR(32) NOT NULL
+    `password` VARCHAR(128) NOT NULL,
+    PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `schools` (
-
+CREATE TABLE IF NOT EXISTS `schools` (
+    `id` INT AUTO_INCREMENT,
+    `name` VARCHAR(32) NOT NULL,
+    `type` ENUM('Primary', 'Secondary', 'Higher Education'),
+    `location` VARCHAR(32) NOT NULL,
+    `year_founded` INT UNSIGNED NOT NULL,
+    PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `companies` (
-
+CREATE TABLE IF NOT EXISTS `companies` (
+    `id` INT AUTO_INCREMENT,
+    `name` VARCHAR(32) NOT NULL,
+    `industry` ENUM('Technology', 'Education', 'Business'),
+    `location` VARCHAR(32) NOT NULL,
+    PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `connections` (
+CREATE TABLE `people_connections` (
+    `id` INT AUTO_INCREMENT,
+    `user_id` INT NOT NULL,
+    `following_id` INT NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`id`),
+    FOREIGN KEY (`following_id`) REFERENCES `users`(`id`),
+    CONSTRAINT `unique_connection` UNIQUE (`user_id`, `following_id`)
+);
 
+CREATE TABLE `school_connections` (
+    `id` INT AUTO_INCREMENT,
+    `user_id` INT NOT NULL,
+    `school_id` INT NOT NULL,
+    `start_date` DATE NOT NULL,
+    `end_date` DATE NOT NULL,
+    `degree_type` VARCHAR(32),
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`id`),
+    FOREIGN KEY (`school_id`) REFERENCES `schools`(`id`),
+    CONSTRAINT `unique_school_connection` UNIQUE (`user_id`, `school_id`)
+);
+
+CREATE TABLE `company_connections` (
+    `id` INT AUTO_INCREMENT,
+    `user_id` INT NOT NULL,
+    `company_id` INT NOT NULL,
+    `start_date` DATE NOT NULL,
+    `end_date` DATE NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`user_id`) REFERENCES `Users`(`id`),
+    FOREIGN KEY (`company_id`) REFERENCES `Companies`(`id`),
+    CONSTRAINT `unique_company_connection` UNIQUE (`user_id`, `company_id`)
 );
